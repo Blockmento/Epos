5bda80da7cfb0f91679cb0da4e472b2e6d13858e
Blockmento <75256857+Blockmento@users.noreply.github.com>
2021-04-15 23:17:23 +0200

    Nearest edge calculation for second hexagon

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 console.log("Starte");
-const Edges = [[0,2/3],[1,1/3],[1,-1/3],[0,-2/3],[-1,-1/3],[1,1/3]]
+const Edges = [[0,2/3],[1,1/3],[1,-1/3],[0,-2/3],[-1,-1/3],[1,1/3]];
 /* 
  0
 5/\1
@@ -9,33 +9,45 @@ const Edges = [[0,2/3],[1,1/3],[1,-1/3],[0,-2/3],[-1,-1/3],[1,1/3]]
  */
 
 function nearest_edges(point_a,point_b) { //Calculats the tow nearest edges of tow hexagones
-    var smalest_dist = [[1000000,0],[1000000,0]];
+    let smalest_dist = [[1000000,0],[1000000,0]];
     for (let i = 0; i < Edges.length; i++) {
         //calculate the position of the curent edge
-        const edge = Edges[i];
-        edge[0] = parseInt(point_a[0]) + edge[0]; //this changes the Edges variable for some reason
-        edge[1] = parseInt(point_a[1]) + edge[1];
+        let edge = Edges[i];
+        //edge[0] = parseInt(point_a[0]) + edge[0]; //this changes the Edges variable for some reason
+        //edge[1] = parseInt(point_a[1]) + edge[1];
 
         //calculate the distance between calculated edge and the center of point_b
-        const distance = Math.sqrt(Math.pow(Math.abs(edge[0]-point_b[0]),2)+Math.pow(Math.abs(edge[1]-point_b[1]),2));
+        const distance = Math.sqrt(Math.pow(Math.abs((parseInt(point_a[0]) + edge[0])-point_b[0]),2) + Math.pow(Math.abs((parseInt(point_a[1]) + edge[1])-point_b[1]),2));
 
         //check if this distance is the smales distance
         if (distance < smalest_dist[0][0]) {
             smalest_dist [0] = [distance,i];
         }
-        //todo if to edges have the same distance, save them both
+        else if (distance == smalest_dist[0][0]) {        //todo if to edges have the same distance, save them both
+            return;
+        }
     }
     var edge_a = [0,0];
     edge_a[0] = parseInt(point_a[0]) + Edges[smalest_dist[0][1]] [0];
     edge_a[1] = parseInt(point_a[1]) + Edges[smalest_dist[0][1]] [1];
-    console.log(edge_a);
+
     for (let i = 0; i < Edges.length; i++) {
         //calculate the position of the curent edge
         const edge = Edges[i];
-        edge[0] = parseInt(point_a[0]) + edge[0];
-        edge[1] = parseInt(point_a[1]) + edge[1];
+        //edge[0] = parseInt(point_a[0]) + edge[0];
+        //edge[1] = parseInt(point_a[1]) + edge[1];
         
         //calculate the distance between edge_a and edge
+        const distance = Math.sqrt(Math.pow(Math.abs((parseInt(point_b[0]) + edge[0])-edge_a[0]),2) + Math.pow(Math.abs((parseInt(point_b[1]) + edge[1])-edge_a[1]),2));
+
+        //check if this distance is the smales distance
+        if (distance < smalest_dist[1][0]) {
+            smalest_dist [1] = [distance,i];
+        }
+        else if (distance == smalest_dist[1][0]) {        //todo if to edges have the same distance, save them both
+            console.log("same");
+            //return;
+        }
     }
     console.log(smalest_dist);
 }
